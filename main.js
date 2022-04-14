be5328b2da1456ec4009bded16886f0c4fea99da
guoxudong01 <guoxudong01@corp.netease.com>
2022-04-14 22:07:23 +0800

    add test js code

--- main.js
+++ main.js
@@ -20,10 +20,21 @@ console.log(typeof(e));
 var f = null;
 console.log(typeof(f));
 
+//how different with array and object?
 let arr = ["a", "b", "c"];
 console.log(typeof(arr));
+
 
 for (var i = 0; i < arr.length; i++)
 {
     console.log(arr[i]);
 }
+
+arr.push("d");
+
+var num = 0;
+while(true)
+{
+    console.log("while" + num++);
+}
+
